d82241caa135db370f0e09adacda9ca484740f37
Meir Taffel <artgardening@gmail.com>
2017-08-29 15:03:25 +0000

    finish checkpoint 7 associations

--- address-bloc/models/entry.rb
+++ address-bloc/models/entry.rb
@@ -4,6 +4,8 @@ class Entry < BlocRecord::Base
   # These must be accessors since we mutate them
   attr_accessor :name, :phone_number, :email
 
+  belongs_to :address_book
+
   def to_s
     "Name: #{name}\nPhone Number: #{phone_number}\nEmail: #{email}"
   end
